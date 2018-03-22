c40fb31d0bd452f151714f8d0d6ce7e414e2404d
Андрей Власов <andrey11vlasov@yandex.ru>
2018-03-23 02:22:31 +0300

    add a comment

--- AnagramFinder/MultiThreadAnagrams.cs
+++ AnagramFinder/MultiThreadAnagrams.cs
@@ -9,16 +9,18 @@ namespace AnagramFinder
     class MultiThreadAnagrams
     {
         public static List<string> GetAnagrams(string word, List<string> dictionary, int threadCount)
-        {
+        {          
             int step = dictionary.Count / threadCount;
             var results = new List<string>[threadCount];
             var result = new List<string>();
             Parallel.For(0, threadCount, i =>
             {
-                var part = i < threadCount - 1 ?
+                // Разбиение словаря на части, для последней части (i = threadCount - 1)
+                // пропускается Take, и Skip идет до конца
+                var partOfDict = i < threadCount - 1 ?
                     dictionary.Skip(i * step).Take(step).ToList() :
-                    dictionary.Skip(i * step).ToList();
-                results[i] = FindAnagramsRefactored.AnagramsParser(word, part);
+                    dictionary.Skip(i * step).ToList();               
+                results[i] = FindAnagramsRefactored.AnagramsParser(word, partOfDict);
             });
             foreach (var arr in results)
             {
